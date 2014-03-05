82b715f29771aaa5142f13a2ffb09ad034a9a9f0
Gary Haran <gary.haran@gmail.com>
2014-03-05 10:33:08 -0500

    fixing linking

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -9,7 +9,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
+files="bashrc vimrc bash_profile" # list of files/folders to symlink in homedir
 
 ##########
 
